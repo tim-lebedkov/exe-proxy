b34a283bfb772b9d3d119a70270cf606daee5ef9
tim-lebedkov <tim.lebedkov@gmail.com>
2020-03-05 12:40:14 -0800

    build also with JavaScript

--- AppVeyorBuild.bat
+++ AppVeyorBuild.bat
@@ -19,6 +19,19 @@ cmake C:\projects\exe-proxy -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=MinSizeRel
 if %errorlevel% neq 0 exit /b %errorlevel%
 mingw32-make.exe -j 2
 if %errorlevel% neq 0 exit /b %errorlevel%
+C:\msys64\mingw32\bin\strip exeproxy.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+set where=c:\Builds\exe-proxy-32-minsizerel-js
+mkdir %where%
+cd %where%
+set path=C:\msys64\mingw32\bin;C:\Program Files (x86)\CMake\bin
+cmake C:\projects\exe-proxy -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=MinSizeRel -DWITH_JAVASCRIPT=ON
+if %errorlevel% neq 0 exit /b %errorlevel%
+mingw32-make.exe -j 2
+if %errorlevel% neq 0 exit /b %errorlevel%
+C:\msys64\mingw32\bin\strip exeproxy.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 set where=c:\Builds\exe-proxy-64-minsizerel
 mkdir %where%
@@ -28,15 +41,30 @@ cmake C:\projects\exe-proxy -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=MinSizeRel
 if %errorlevel% neq 0 exit /b %errorlevel%
 mingw32-make.exe -j 2
 if %errorlevel% neq 0 exit /b %errorlevel%
+C:\msys64\mingw64\bin\strip exeproxy.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-set path=%initial_path%
+set where=c:\Builds\exe-proxy-64-minsizerel-js
+mkdir %where%
+cd %where%
+set path=C:\msys64\mingw64\bin;C:\Program Files (x86)\CMake\bin
+cmake C:\projects\exe-proxy -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=MinSizeRel -DWITH_JAVASCRIPT=ON
+if %errorlevel% neq 0 exit /b %errorlevel%
+mingw32-make.exe -j 2
+if %errorlevel% neq 0 exit /b %errorlevel%
+C:\msys64\mingw64\bin\strip exeproxy.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 set where=c:\release
 mkdir %where%
 cd %where%
 copy c:\Builds\exe-proxy-32-minsizerel\exeproxy.exe exeproxy.exe
 if %errorlevel% neq 0 exit /b %errorlevel%
+copy c:\Builds\exe-proxy-32-minsizerel-js\exeproxy.exe exeproxy-js.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
 copy c:\Builds\exe-proxy-64-minsizerel\exeproxy.exe exeproxy64.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
+copy c:\Builds\exe-proxy-64-minsizerel-js\exeproxy.exe exeproxy-js64.exe
 if %errorlevel% neq 0 exit /b %errorlevel%
 copy "C:\projects\exe-proxy\README.md" .
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -47,6 +75,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 7z a C:\Artifacts\exeproxy-%version%.zip * -mx9	
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+set path=%initial_path%
 appveyor PushArtifact C:\Artifacts\exeproxy-%version%.zip
 if %errorlevel% neq 0 exit /b %errorlevel%
 
